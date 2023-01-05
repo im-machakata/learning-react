73b579f2b3c88d1107a4dc5c50ea19befdf8ad15
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-01-05 23:47:13 +0200

    refactor: introduction to components

--- src/h1tag.jsx
+++ src/h1tag.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const welcomeMessage = "Hello there! I'm Isaac Machakata and I'm currently learning some React basics.";
-const h1tag = "<h1>{welcomeMessge}</h1>";
-export default h1tag;
+function HelloThere(){
+    return <h1>Hello there! I'm Isaac Machakata and I'm currently learning some React basics.</h1>;
+}
+
+const HelloThereVar = <HelloThere/>;
+export default HelloThereVar;
