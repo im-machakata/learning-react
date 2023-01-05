2cebe7824f1478bfe0ba6d48af2d28dba7438cdb
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-01-05 23:54:15 +0200

    refactor: implement states

--- src/h1tag.jsx
+++ src/h1tag.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: "Hello there! I'm Isaac Machakata and I'm currently learning some React basics.",
+    };
+  }
   render() {
     return (
-      <h1>
-        Hello there! I'm Isaac Machakata and I'm currently learning some React
-        basics.
-      </h1>
+      <h1>{this.state.msg}</h1>
     );
   }
 }
