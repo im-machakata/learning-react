8f19acc5f9112c37a9d5e3e3378b55adfb8b0376
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-01-05 23:37:22 +0200

    feature:render text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+ReactDOM.render(
+    <h1>Hello there! I'm Isaac</h1>,
+    document.getElementById('root')
+);
