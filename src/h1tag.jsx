0cb08a812e9c989c206972bfbb08cb955c261504
Isaac Machakata <108731754+im-machakata@users.noreply.github.com>
2023-01-05 23:43:08 +0200

    refactor: use jsx file instead

---
+++ src/h1tag.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const welcomeMessage = "Hello there! I'm Isaac Machakata and I'm currently learning some React basics.";
+const h1tag = "<h1>{welcomeMessge}</h1>";
+export default h1tag;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import h1tag from './h1tag.jsx';
 
-const welcomeMessage = "Hello there! I'm Isaac Machakata and I'm currently learning some React basics.";
-const h1Tag = "<h1>{welcomeMessge}</h1>";
 ReactDOM.render(
-    h1Tag,
+    h1tag,
     document.getElementById('root')
 );
